9ede736bf6cd7e2a30b36889a4c5c662cd274c64
sapar <sapar@meyvndigital.co.uk>
2021-10-20 22:08:10 +0300

    rubocop -a

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,9 @@
 class Task < ApplicationRecord
-  belongs_to :author, class_name: "User"
-  belongs_to :assignee, class_name: "User", optional: true
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
 
-  has_many :my_tasks, class_name: "Task", foreign_key: :author_id
-  has_many :assigned_tasks, class_name: "Task", foreign_key: :assignee_id
+  has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
+  has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
 
   validates :name, presence: true
   validates :description, presence: true
