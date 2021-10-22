6f28ad178cdac4f5d973001f3b7824c07f9a9620
sapar <sapar@meyvndigital.co.uk>
2021-10-22 23:55:19 +0300

    integrate state-machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,34 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine :state, :initial => :new_task do
+    event :in_development do
+      transition :new_task => :in_development
+    end
+
+    event :ready_for_review do
+      transition :in_development => :in_qa
+    end
+
+    event :ready_for_code_review do
+      transition :in_qa => :in_code_review
+    end
+
+    event :return_to_development do
+      transition [:in_qa, :in_code_review] => :in_development
+    end
+
+    event :ready_for_release do
+      transition :in_code_review => :ready_for_release
+    end
+
+    event :released do
+      transition :ready_for_release => :released
+    end
+
+    event :archived do
+      transition [:new_task, :released] => :archived
+    end
+  end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,8 +1,84 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  test 'create' do
-    task = create(:task)
-    assert task.persisted?
+  def setup
+    @task = create(:task)
   end
+  test 'create' do
+    assert @task.persisted?
+  end
+
+  test 'initial state should be :new_task' do
+    assert_equal 'new_task', @task.state
+  end
+
+  test 'new task can be moved to :in_development' do
+    @task.in_development
+    assert_equal 'in_development', @task.state
+  end
+
+  test 'new task can be moved to :archived' do
+    @task.archived
+    assert_equal 'archived', @task.state
+  end
+
+  test ':in_development task can be moved to :in_qa' do
+    @task.in_development
+    @task.ready_for_review
+    assert_equal 'in_qa', @task.state
+  end
+
+  test ':in_qa task can be moved to :in_code_review' do
+    @task.in_development
+    @task.ready_for_review
+    @task.ready_for_code_review
+    assert_equal 'in_code_review', @task.state
+  end
+
+  test ':in_qa task can be moved back to :in_development' do
+    @task.in_development
+    @task.ready_for_review
+    @task.in_development
+  end
+
+  test ':in_code_review task can be moved to :ready_for_release' do
+    @task.in_development
+    @task.ready_for_review
+    @task.ready_for_code_review
+    @task.ready_for_release
+    assert_equal 'ready_for_release', @task.state
+  end
+
+  test ':in_code_review task can be moved back to :in_development' do
+    @task.in_development
+    @task.ready_for_review
+    @task.ready_for_code_review
+    @task.in_development
+  end
+
+  test ':ready_for_release task can be moved to :released' do
+    @task.in_development
+    @task.ready_for_review
+    @task.ready_for_code_review
+    @task.ready_for_release
+    @task.released
+    assert_equal 'released', @task.state
+  end
+
+  test ':released task can be moved to :archived' do
+    @task.in_development
+    @task.ready_for_review
+    @task.ready_for_code_review
+    @task.ready_for_release
+    @task.released
+    @task.archived
+    assert_equal 'archived', @task.state
+  end
+  # test ':in_development task can be moved to :archived' do
+  #   @task.start_progress
+  #   @task.archived
+  #   assert_equal 'archived', @task.state
+  # end
+
+
 end
