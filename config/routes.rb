79057147745de2eb2e12cde20210b604ce1d634f
sapar <sapar@meyvndigital.co.uk>
2021-10-24 13:48:51 +0300

    add material-design-lit, config simple_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root :to => "web/boards#show"
+
+  scope module: :web do
+    resource :board, only: :show
+    resource :session, only: :new
+  end
+  namespace :web do
+    get 'sessions/new'
+  end
+  namespace :web do
+    get 'boards/show'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
