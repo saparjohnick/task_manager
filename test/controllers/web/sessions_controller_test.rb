c0dfba5d148e3ffbbf9c1aca5cde9c73c71e52ed
sapar <sapar@meyvndigital.co.uk>
2021-10-27 23:13:00 +0300

    sessions -- add :create and :destroy, specs, form object, slim view

--- test/controllers/web/sessions_controller_test.rb
+++ test/controllers/web/sessions_controller_test.rb
@@ -5,4 +5,20 @@ class Web::SessionsControllerTest < ActionDispatch::IntegrationTest
     get new_session_path
     assert_response :success
   end
+
+  test 'should post create' do
+    password = generate(:string)
+    user = create(:user, { password: password })
+    attrs = {
+      email: user.email,
+      password: password,
+    }
+    post session_path, params: { session_form: attrs }
+    assert_response :redirect
+  end
+
+  test 'should define destroy' do
+    delete session_path
+    assert_response :redirect
+  end
 end
