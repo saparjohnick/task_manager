7907f5016fa1744b66d1ff0a307a84e3ba7b98f6
sapar <sapar@meyvndigital.co.uk>
2021-10-28 00:39:11 +0300

    rubocop -a

--- app/controllers/concerns/auth_helper.rb
+++ app/controllers/concerns/auth_helper.rb
@@ -1,6 +1,6 @@
 module AuthHelper
   extend ActiveSupport::Concern
-  
+
   def sign_in(user)
     session[:user_id] = user.id
   end
@@ -14,11 +14,12 @@ module AuthHelper
   end
 
   def authenticate_user!
-    redirect_to new_session_path unless signed_in?
+    redirect_to(new_session_path) unless signed_in?
   end
 
   def current_user
     return if session[:user_id].blank?
+
     @_current_user = User.find_by(id: session[:user_id])
   end
 end
