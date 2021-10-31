efa81aeac189580e23e9b6668abbcd0fe1d1b3e4
sapar <sapar@meyvndigital.co.uk>
2021-10-31 22:52:58 +0300

    add index action

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,11 @@
 class TasksController < Api::V1::ApplicationController
+  def index 
+    task = Task.all
+               .ransack(ransack_params)
+               .result
+               .page(page)
+               .per(per_page)
+
+    respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+  end
 end
