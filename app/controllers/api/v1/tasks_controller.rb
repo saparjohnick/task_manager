f8a42f200872e1ae8cfa83e48b88e34818586e25
sapar <sapar@meyvndigital.co.uk>
2021-11-30 21:16:39 +0300

    sort on application_controller instead of tasks_controller, remove unused param from delete js

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::TasksController < Api::V1::ApplicationController
   def index
-    search = Task.all.ransack(ransack_params)
-    search.sorts = RANSACK_DEFAULT_SORT
-    tasks = search.
-      result.
-      page(page).
-      per(per_page)
+    tasks = Task.all.
+                  ransack(ransack_params).
+                  result.
+                  page(page).
+                  per(per_page)
 
     respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
   end
