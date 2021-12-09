9dce32e0db5316cbbfc664ff065eff77fc7f3a24
sapar <sapar@meyvndigital.co.uk>
2021-12-09 23:35:53 +0300

    try to fix lcov issue

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,9 +9,9 @@ if ENV['COVERAGE'] == true
   SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
   SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
   SimpleCov::Formatter::LcovFormatter.config do |c|
-    c.output_directory = './' # default: "coverage/lcov"
+    c.output_directory = 'tmp/' # default: "coverage/lcov"
     c.lcov_file_name = 'lcov.info' # default: "YOUR_PROJECT_NAME.lcov"
-    c.single_report_path = './lcov.info'
+    c.single_report_path = 'tmp/lcov.info'
   end
   SimpleCov.start
 end
