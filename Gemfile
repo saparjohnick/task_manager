ab4ca4a546fd7c442bf6387a5b2240c75fd35dfa
sapar <sapar@meyvndigital.co.uk>
2021-12-09 12:17:10 +0300

    deploy

--- Gemfile
+++ Gemfile
@@ -58,6 +58,8 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  gem 'simplecov', require: false
+  gem 'simplecov-lcov', require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,18 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+if ENV['COVERAGE']
+  require 'simplecov'
+  require 'simplecov-lcov'
+
+  SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+  SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+  SimpleCov::Formatter::LcovFormatter.config do |c|
+    c.single_report_path = 'coverage/lcov/lcov.info'
+  end
+  SimpleCov.start
+end
+
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
   include AuthHelper
