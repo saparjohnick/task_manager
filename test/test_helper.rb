0d5e2b5872ea0944bf2b4afbeeb7536f0116645c
sapar <sapar@meyvndigital.co.uk>
2021-12-09 23:26:01 +0300

    try to fix lcov issue

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-if ENV['CI']
+if ENV['COVERAGE'] == true
   require 'simplecov'
   require 'simplecov-lcov'
 
