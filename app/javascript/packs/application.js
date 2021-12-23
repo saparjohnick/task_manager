9574ad522d931cc9914ba88209a68786849730d1
sapar <sapar@meyvndigital.co.uk>
2021-12-23 13:18:37 +0300

    extract_css: true

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import 'material-design-lite/material.js';
-import '../../assets/stylesheets/application.css';
+import '../../assets/stylesheets';
 import WebpackerReact from 'webpacker-react';
 
 import App from '../App';
