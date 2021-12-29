0b2100a80c6707408f52059527bb79125fef3ae8
sapar <sapar@meyvndigital.co.uk>
2021-12-30 00:38:59 +0300

    prod env fixed

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,20 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-const environment = require('./environment')
+const environment = require('./environment');
 
-module.exports = environment.toWebpackConfig()
+environment.plugins.append(
+  'BundleAnalyzer',
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: true,
+  })
+);
+
+const config = environment.toWebpackConfig();
+config.devtool = 'none';
+config.performance = {
+  maxAssetSize: 250000,
+};
+
+module.exports = config;
